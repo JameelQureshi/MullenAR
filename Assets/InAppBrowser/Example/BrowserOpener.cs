4fe7fee4b9917e2309e2608a0ef314fa90183cc8
Jameel Qureshi <jameelqureshi2013@gmail.com>
2019-08-23 17:52:50 +0500

    After Adding InApp Browser

---
+++ Assets/InAppBrowser/Example/BrowserOpener.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrowserOpener : MonoBehaviour {
+    private void Start()
+    {
+        OnButtonClicked();
+    }
+
+    public string pageToOpen = "https://pano.autohome.com.cn/car/pano/34383";
+
+	// check readme file to find out how to change title, colors etc.
+	public void OnButtonClicked() {
+		InAppBrowser.DisplayOptions options = new InAppBrowser.DisplayOptions();
+		options.displayURLAsPageTitle = false;
+		options.pageTitle = "Qianto50 K50 360View";
+
+		InAppBrowser.OpenURL(pageToOpen, options);
+	}
+
+	public void OnClearCacheClicked() {
+		InAppBrowser.ClearCache();
+	}
+}
