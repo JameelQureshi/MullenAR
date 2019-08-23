5685bf7434c9ce131af4516bd331e68ee9faadc5
Jameel Qureshi <jameelqureshi2013@gmail.com>
2019-08-23 10:32:33 +0500

    Latest Design Menu Ready
    After latest design finishing the menu and splash

---
+++ Assets/Scripts/ImageTargetEvents.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+namespace Vuforia
+{
+public class ImageTargetEvents : MonoBehaviour,ITrackableEventHandler {
+		private TrackableBehaviour mTrackableBehaviour;
+        public UnityEvent onTargetFound;
+        public UnityEvent onTargetLost;
+	// Use this for initialization
+	void Start () {
+
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+
+                onTargetFound.Invoke();
+                Debug.Log("Target Found");
+
+        }
+		else
+		{
+
+                onTargetLost.Invoke();
+                Debug.Log("Target Lost");
+            }
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/SplashManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SplashManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadScene",2.0f);
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
